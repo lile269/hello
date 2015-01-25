e257950ded4a4aafae57378ee0063c00ad2b9a66
lile269 <lile269@foxmail.com>
2015-01-25 22:01:05 +0800

    branch2 delete

--- HelloWorld/StepViewController.m
+++ HelloWorld/StepViewController.m
@@ -17,8 +17,8 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-     NSLog(@"Bye branch1");
     NSLog(@"Bye branch2");
+     NSLog(@"delete branch3");
     
     
 	// Do any additional setup after loading the view, typically from a nib.
